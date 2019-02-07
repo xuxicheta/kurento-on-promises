e98b92e8b44ed68eb7c974c2130ee63d6d1fd029
Anton Shvets <antshvets@gmail.com>
2019-02-07 23:35:26 +0300

    actualize env

--- server/app.js
+++ server/app.js
@@ -2,23 +2,18 @@
 const httpErrors = require('http-errors');
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
 
 const { router } = require('./routes/index.route');
 const { config } = require('./config');
 
 const app = express();
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(config.rootDir, 'client', 'static')));
 app.use(express.static(path.resolve(config.rootDir, 'client', 'dist')));
 app.use(express.static(path.resolve(config.rootDir, 'files')));
 app.use(router);
-app.get('favicon.ico', (req, res) => {
-  res.send('');
-});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
